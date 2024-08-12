0fe2021ae83a840fc40afcef2801f1df63615702
lenhard22 <115093271+lenhard22@users.noreply.github.com>
2024-08-12 08:26:47 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,19 @@
+let resp = window.document.getElementById('saida')
+
+function ação1() {
+    resp.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function ação2() {
+    resp.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+
+function ação3() {
+    resp.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+
+function ação4() {
+    resp.innerHTML += '<p>Clicou no quarto botão</p>'
+}
